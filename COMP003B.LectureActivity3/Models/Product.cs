b07b3607984f1430639f751de2d460a8187b6b76
PedroGF27 <coolcatdopedog@gmail.com>
2025-03-31 18:11:45 -0700

    added product class

--- COMP003B.LectureActivity3/Models/Product.cs
+++ COMP003B.LectureActivity3/Models/Product.cs
@@ -4,9 +4,17 @@ namespace COMP003B.LectureActivity3.Models
 {
 	public class Product
 	{
+		// Add necessary atributes
 		public int Id { get; set; }
 
 		[Required]
-		[StringLength()]
+		[StringLength(100)]
+		public string Name { get; set; }
+
+		[Range(0.01, 10000)]
+		public string Price { get; set; }
+
+		[Required]
+		public string Category { get; set; }
 	}
 }
