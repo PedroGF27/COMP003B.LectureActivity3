b173d06ff2eb1b735847709e5a225f95e851aa62
PedroGF27 <coolcatdopedog@gmail.com>
2025-03-31 18:06:30 -0700

    Add project files.

---
+++ COMP003B.LectureActivity3/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.LectureActivity3.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength()]
+	}
+}
