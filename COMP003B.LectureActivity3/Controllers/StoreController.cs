67800f764b8d382020f544c4082656c189b344d8
PedroGF27 <coolcatdopedog@gmail.com>
2025-03-31 18:27:55 -0700

    Added a product controller

---
+++ COMP003B.LectureActivity3/Controllers/StoreController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.LectureActivity3.Controllers
+{
+	[Route("store")]
+	public class StoreController : Controller
+	{
+		[Route("product/{id:int}")]
+		public IActionResult ViewProduct(int id)
+		{
+			return View(id);
+		}
+	}
+}
